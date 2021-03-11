74a287e79e3cc10b6b3b5f9ebc2ce94b2e60ce3f
ro <ronaldmartinez@bcp.com.pe>
2021-03-11 00:28:25 -0500

    Initial Commit

---
+++ retoiOS-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  retoiOS-MVVM
+//
+//  Created by ro martinez on 3/11/21.
+//  Copyright © 2021 ro martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
